1db5051bd95a50ef9dd48f600e19593c046d36cd
Fabio Moretão <fabiojosemoretaodesouza@gmail.com>
2025-08-26 11:14:36 -0300

    feat(component): cria um header de navegacao entre as paginas

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,18 +1,38 @@
 import { Link, Outlet } from "react-router-dom";
+import styles from "./layout.module.css"
+import { RiDashboardHorizontalFill } from "react-icons/ri";
+import { BsPlusSquareFill } from "react-icons/bs";
+import { FaListUl } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Layout() {
+    const [clicked, setClicked] = useState('/')
     return (
         <div>
-            <header>
-                <nav>
-                    <Link to='/'>Dashboard</Link>
-                    <Link to='/new-product'>Adicionar Produto</Link>
-                    <Link to='/products-list'>Lista de Produtos</Link>
-                </nav>
+            <header className={styles.header}>
+                <Link
+                    to='/'
+                    className={`${styles.link}  ${clicked === '/' ? styles.linkClicked : ''}`}
+                    onClick={() => setClicked('/')}
+                >
+                    <RiDashboardHorizontalFill />Dashboard
+                </Link>
+                <Link
+                    to='/new-product'
+                    className={`${styles.link}  ${clicked === '/new-product' ? styles.linkClicked : ''}`}
+                    onClick={() => setClicked('/new-product')}
+                ><BsPlusSquareFill />Adicionar Produtos
+                </Link>
+                <Link
+                    to='/products-list'
+                    className={`${styles.link}  ${clicked === '/products-list' ? styles.linkClicked : ''}`}
+                    onClick={() => setClicked('/products-list')}
+                ><FaListUl />Lista de Produtos
+                </Link>
             </header>
             <main>
                 <Outlet />
             </main>
-        </div>
+        </div >
     )
 }
