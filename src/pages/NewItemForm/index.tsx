19151783552de1cbec6c4d8f9879de305f84ed29
Fabio Moretão <fabiojosemoretaodesouza@gmail.com>
2025-08-28 12:09:48 -0300

    style(newItemForm): adiciona estilo no formulario de novo item

--- src/pages/NewItemForm/index.tsx
+++ src/pages/NewItemForm/index.tsx
@@ -3,6 +3,8 @@
 import Form from '../../components/Form'
 import type { Product } from '../../types'
 import { FaCircleArrowLeft } from 'react-icons/fa6'
+import styles from "./styles.module.css"
+import { Link } from 'react-router-dom'
 
 type NewItemFormProps = {
     addProduct: (data: Omit<Product, "id" | "date">) => void
@@ -11,10 +13,14 @@ type NewItemFormProps = {
 export default function NewItemForm({ addProduct }: NewItemFormProps) {
     return (
         <div>
-            <h2>Cadastro de Novo Produto</h2>
-            <div>
-                <FaCircleArrowLeft />
-                <p>Voltar</p>
+            <div className={styles.header}>
+                <h2>Cadastro de Novo Produto</h2>
+                <Link to='/' style={{ textDecoration: 'none' }}>
+                    <div className={styles.return}>
+                        <FaCircleArrowLeft fontSize={"32px"} />
+                        <p>Voltar</p>
+                    </div>
+                </Link>
             </div>
             <Form
                 onSubmit={addProduct}
